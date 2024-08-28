00dfe7b98de8169ae71b561b42789339e6cc68b5
SajidPervez <sajidpervez@gmail.com>
2024-08-28 13:33:56 +1000

    made changes to [slug].astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,6 +15,20 @@ const blogCollection = defineCollection({
   })
 });
 
+const authorCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    author: z.string(),
+    isDraft: z.boolean(),
+    pubDate: z.string().transform(str => new Date(str)),
+    tags: z.array(z.string()),
+    cover: image().optional(),
+    altText: z.string(),
+    canonicalURL: z.string().url(),
+  })
+});
 export const collections = {
   'blog': blogCollection,
+  'authors': authorCollection,
 };
