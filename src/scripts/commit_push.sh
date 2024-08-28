2b57a2e77e2a366c7055b40839857fb3a65e9c97
SajidPervez <sajidpervez@gmail.com>
2024-08-28 12:57:58 +1000

    fixed the [slug].astro error with preender=true

--- src/scripts/commit_push.sh
+++ src/scripts/commit_push.sh
@@ -7,21 +7,25 @@ function exit_if_failed() {
     exit 1
   fi
 }
+# Check if there are any changes to commit
+if [ -n "$(git status --porcelain)" ]; then
+  # Prompt the user for a commit message
+  echo "Enter your commit message:"
+  read commit_message
 
-# Prompt the user for a commit message
-echo "Enter your commit message:"
-read commit_message
+  # Add all changes to the staging area
+  git add .
+  exit_if_failed
 
-# Add all changes to the staging area
-git add .
-exit_if_failed
+  # Commit the changes with the provided message
+  git commit -m "$commit_message"
+  exit_if_failed
 
-# Commit the changes with the provided message
-git commit -m "$commit_message"
-exit_if_failed
+  # Push the changes to the remote repository on the current branch
+  git push
+  exit_if_failed
 
-# Push the changes to the remote repository on the current branch
-git push
-exit_if_failed
-
-echo "Code committed and pushed to GitHub successfully!"
+  echo "Code committed and pushed to GitHub successfully!"
+else
+  echo "No changes to commit."
+fi
